883f03608b5dff7deb97dcded808476a9564cee1
Corne2dutoit <108460684+Corne2dutoit@users.noreply.github.com>
2024-02-28 14:26:26 +0200

    telos test

--- src/config/configuredChains.ts
+++ src/config/configuredChains.ts
@@ -1,22 +1,22 @@
 // import EOS from 'src/config/chains/eos';
-// import Telos from 'src/config/chains/telos';
+import Telos from 'src/config/chains/telos';
 // import UX from 'src/config/chains/ux';
 // import Wax from 'src/config/chains/wax';
 import KOY from 'src/config/chains/koy';
 import KoyTestnet from 'src/config/chains/koy-testnet';
 
-// import TelosTestnet from 'src/config/chains/telos-testnet';
+import TelosTestnet from 'src/config/chains/telos-testnet';
 // import Jungle from 'src/config/chains/jungle';
 
 import { ChainsConfig } from 'src/types/ChainsConfig';
 
 const chains: ChainsConfig = {
     // mainnets: [new EOS('eos'), new Telos('telos'), new UX('ux'), new Wax('wax'), new KOY('koy')],
-    // mainnets: [new KOY('koy'), new Telos('telos')],
-    mainnets: [new KOY('koy')],
+    mainnets: [new KOY('koy'), new Telos('telos')],
     // mainnets: [new KOY('koy')],
-    // testnets: [new KoyTestnet('koy-testnet'), new TelosTestnet('telos-testnet')],
-    testnets: [new KoyTestnet('koy-testnet')],
+    // mainnets: [new KOY('koy')],
+    testnets: [new KoyTestnet('koy-testnet'), new TelosTestnet('telos-testnet')],
+    // testnets: [new KoyTestnet('koy-testnet')],
 };
 
 export default chains;
